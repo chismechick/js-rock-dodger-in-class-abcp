e6c369caf81ef8174c10a320c157fadff4a5f407
Jennifer K <jennifer@chismechick.com>
2018-01-21 19:10:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -93,9 +93,7 @@ function moveDodgerLeft() {
   // remove 4 px from the curent dodger left style
   // find the current location/number of pixels
   // subtract 4 from that number of pixels
- var myPositionInPixels
-
-  $("#dodger").style.left
+ var myPositionInPixels = $("#dodger").style.left
   window.requestAnimationFrame(function() {
     const left = positionToInteger(DODGER.style.left)
 
